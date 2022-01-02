ac4aaae451b820d1f1ae56e0a4b15db6bb6e3498
will rost <62968380+iamwillrost@users.noreply.github.com>
2022-01-01 19:57:04 -0600

    addes more elements

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var h1 = document.createElement("P");
-h1.innerHTML = "this is a paragraphe, duhhh";
-document.body.appendChild(h1);
+var P1 = document.createElement("P");
+var P2 = document.createElement("P");
+var h1 = document.createElement("h1");
+var header = document.createElement("header");
+var nav = document.createElement("nav");
+var footer = document.createElement("footer");
+var div = document.createElement("div");
+
+P1.innerHTML = "this is a paragraph, duhhh";
+document.body.appendChild(P1);
+
+P2.innerHTML = "this is paragraph 2";
+document.body.appendChild(P2);
+
+
+
