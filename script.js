db133291fb8ce8a4c2067d6444809b5793211637
will rost <62968380+iamwillrost@users.noreply.github.com>
2022-01-03 02:43:01 -0600

    remove bs from js

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 var P1 = document.createElement("P");
-var P2 = document.createElement("P");
-var h1 = document.createElement("h1");
-var header = document.createElement("header");
-var nav = document.createElement("nav");
-var footer = document.createElement("footer");
-var div = document.createElement("div");
-var ul = document.createElement("ul");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-
-document.body.appendChild(header);
-
-document.header.appendChild(nav);
-
-document.ul.appendChild(li1, li2)
-
-li1.innerHTML= "nav1";
-
-li2.innerHTML = "nav2";
-
-document.nav.appendChile(ul);
-
-P1.innerHTML = "this is a paragraph, duhhh";
-document.body.appendChild(P1);
-
 P2.innerHTML = "this is paragraph 2";
 document.body.appendChild(P2);
 
